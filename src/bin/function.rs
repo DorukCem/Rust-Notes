9153dec929b14a3974114633ad884fe16c23aeeb
Doruk Cem <doruk.cem@gmail.com>
2023-11-30 21:06:48 +0300

    Enough rust for today

--- src/bin/function.rs
+++ src/bin/function.rs
@@ -1,6 +1,8 @@
 fn main() {
    log();
    is_even(2);
+   
+   apply(1, &mult);
 }
 
 // Unlike C/C++, there's no restriction on the order of function definitions
@@ -10,6 +12,19 @@ fn log() {
 }
 
 fn is_even(n : u32) -> bool { 
-   n%2 == 0 // -> can omit return statement if last line is an expression
+   n%2 == 0 // -> can omit return statement if last line is an expression (no ; at the end)
 }
 
+fn mult(n: u32 ) -> u32 {
+   n*2
+}
+
+// Higher order function
+fn apply(n: u32, f: &dyn Fn(u32) -> u32) -> u32{
+   return f(n);
+}
+
+// Diverging functions never return. They are marked using !, which is an empty type.
+fn _foo() -> ! {
+   panic!("This call never returns.");
+}
